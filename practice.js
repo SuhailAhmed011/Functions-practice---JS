50388491b05fa79088b78a18d072e52ac8fada04
Suhail Ahmed <127311098+SuhailAhmed011@users.noreply.github.com>
2023-04-17 00:31:27 +0530

    Add files via upload

---
+++ practice.js
@@ -0,0 +1,78 @@
+
+     /* let age1 = 17;
+      let isVoterId = 'yes';
+
+      if(age>=18){
+      alert("you are eligible for vote");
+      } 
+      else if(isVoterId == 'no'){
+        alert("please make voter id");
+      }
+      else{
+        alert("you are not eligible for vote");
+      }
+
+  
+
+      let age = 18;
+
+      let vote = age>=18 ? "eligible" : "not";
+      console.log(vote);
+
+     
+      let fname = "suhail";
+      let lname = "ahmed";
+
+      let full = `${fname} ${lname}`;
+      console.log(full);
+
+
+      let str1 = "hello guyz my name is suhail ahmed";
+      let store = str1.substr(22, 34);
+      console.log(store);
+
+      let fnamee = "suhail";
+      let lnamee = "ahmed";
+
+      let fullname = fnamee.concat(" " + lnamee);
+      console.log(fullname);
+
+      let str = "hello guyz my name is suhail ahmed";
+      let position = str.replace('suhail', 'shaan');
+      console.log(position);
+
+
+      //let arr = new Array ('suhail', 'sameer', 'shaan', 'jishaan');
+      //console.log(typeof arr);
+
+      let car = [
+        ["ferrari", 250000],
+        ["range rover", 405000],
+        ["porshe", 490888]
+      ];
+
+      console.log(car[1] [0]); //multi dimensinal array initialize
+
+      let cars = ["porshe", "lamborgini", "ferrari", "creta"];
+
+      for(let i=0; i<cars.length; i++){
+        console.log(`car ${i} is a ${cars[i]} \n`);
+      }
+
+      function mulTable(num){
+
+      for(let i=1; i<=10; i++){
+        console.log(`${num} x ${i} = ${num*i}`);
+        
+      }
+
+    }
+
+    mulTable(9);*/
+
+
+    function addNumbers(num1 , num2){
+         console.log(`${num1} + ${num2} = ${num1 + num2}`)
+    }
+
+    addNumbers(4, 9);
